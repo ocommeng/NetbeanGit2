3de3e4f2b00910a316e940f3b406271939d23056
ocommeng <olivier-alain.commenge@ac-toulouse.fr>
2018-01-15 15:17:04 +0100

    Ajout d'un premier cout dans le main

---
+++ main.cpp
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   main.cpp
+ * Author: ocommeng
+ *
+ * Created on 15 janvier 2018, 15:12
+ */
+
+#include <QApplication>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+    cout << "Hello Git World" << endl;
+
+    QApplication app(argc, argv);
+
+    // create and show your widgets here
+
+    return app.exec();
+}
